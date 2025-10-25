887839498d2a127452c8e60e4f630c3c09858cd9
sunyating1 <1613152158@qq.com>
2025-10-25 23:11:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 def isOdd(value):
+    if isinstance(value, bool):
+        return False
     if isinstance(value, int):
-        if value % 2 == 1:
+        if value % 2 != 0:
             return True
-    return False
+        else:
+            return False
+    else:
+        return False
+if __name__ == "__main__":
+    N = input()
+    num = isOdd(N)
+    print(num)
